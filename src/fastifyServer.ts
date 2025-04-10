39149b9f8efa642b424cbe5487adb5828375aea0
Norookezi <norookezi@pandacrp.com>
2025-04-10 17:47:08 +0200

    feat: :sparkles: Detached fastifyServer into a singletone class

--- src/fastifyServer.ts
+++ src/fastifyServer.ts
@@ -1,17 +1,45 @@
-import fastify, { FastifyReply, FastifyRequest } from 'fastify';
-import { config } from 'dotenv';
+import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { config } from 'utils/dotenv.utils';
+
 config();
 
-export const server = fastify();
+export class FastifyServer {
+    public server?: FastifyInstance;
+    private static instance: FastifyServer | null = null;
 
-server.get('/manifest', (_: FastifyRequest, reply: FastifyReply) => {
-    reply.type('application/json').send({
-        version: process.env['MANIFEST_VERSION'] ?? 'v0',
-    });
-});
+    public static initialize(): FastifyServer {
+        if (FastifyServer.instance) {
+            throw new Error('FastifyServer est déjà initialisé');
+        }
+        FastifyServer.instance = new FastifyServer();
+          
+        return FastifyServer.instance;
+    }
+    
+    // Récupère l'instance existante
+    public static getInstance(): FastifyServer {
+        if (!FastifyServer.instance) {
+            throw new Error('FastifyServer non initialisé. Appelez initialize() d\'abord.');
+        }
+        return FastifyServer.instance;
+    }
 
-await server
-    .listen({
-        port: Number(process.env['API_PORT'] ?? '8080'),
-        host: process.env['API_HOST'] ?? '0.0.0.0',
-    });
+    init(): boolean {
+        this.server = fastify({});
+
+        this.server.get('/manifest', (_: FastifyRequest, reply: FastifyReply) => {
+            reply.type('application/json').send({
+                version: process.env['MANIFEST_VERSION'] ?? 'v0',
+            });
+        });
+
+        return !!this.server;
+    }
+
+    async listen(): Promise<string> {
+        return (await this.server!.listen({
+            port: Number(process.env['API_PORT'] ?? '8080'),
+            host: '0.0.0.0',
+        }));
+    }
+}
