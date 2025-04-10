2ed176d70e26971029a874cbdbe2b72620a52678
Norookezi <norookezi@pandacrp.com>
2025-04-10 17:44:46 +0200

    feat: :sparkles: Added webhook route

---
+++ src/routes/webhook.route.ts
@@ -0,0 +1,11 @@
+import { WebhookController } from 'controller/webhook.controller';
+import { FastifyInstance } from 'fastify';
+  
+export class WebhookRoute {
+    routes(fastify: FastifyInstance) {
+        const webhookController: WebhookController = WebhookController.initialize();
+        
+        fastify.post('/', { config: { rawBody: true } }, webhookController.eventProcess);
+    }
+}
+
