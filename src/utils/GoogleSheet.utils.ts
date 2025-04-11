423f68e32a712949385a724be1a2da8df01713ca
Norookezi <norookezi@pandacrp.com>
2025-04-11 23:26:51 +0200

    test

--- src/utils/GoogleSheet.utils.ts
+++ src/utils/GoogleSheet.utils.ts
@@ -7,11 +7,19 @@ interface SheetData {
     values: string[][];
 }
 
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
 export class Sheet {
     async getData(sheetId: string, range: string) {
         const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${process.env['GOOGLE_AUTH_KEY']}`;
         
-        const response = await fetch(url);
+        const response = await fetch(url).catch(async (error) => {
+            console.log(error);
+            await delay(36000000);
+            throw error;
+        });
         
         if (!response.ok) { throw new Error('Failed to fetch data'); }
 
