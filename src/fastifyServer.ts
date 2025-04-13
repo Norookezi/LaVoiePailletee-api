0085252951c1a7497cf5bb59cd520d1371792f2e
Norookezi <norookezi@pandacrp.com>
2025-04-13 13:36:39 +0200

    feat: :lock: Added cors

--- src/fastifyServer.ts
+++ src/fastifyServer.ts
@@ -1,6 +1,10 @@
-import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import fastify, {
+    FastifyInstance,
+    FastifyReply,
+    FastifyRequest,
+} from 'fastify';
 import { config } from 'utils/dotenv.utils';
-
+import cors from '@fastify/cors';
 config();
 
 export class FastifyServer {
@@ -12,14 +16,16 @@ export class FastifyServer {
             throw new Error('FastifyServer est déjà initialisé');
         }
         FastifyServer.instance = new FastifyServer();
-          
+
         return FastifyServer.instance;
     }
-    
+
     // Récupère l'instance existante
     public static getInstance(): FastifyServer {
         if (!FastifyServer.instance) {
-            throw new Error('FastifyServer non initialisé. Appelez initialize() d\'abord.');
+            throw new Error(
+                'FastifyServer non initialisé. Appelez initialize() d\'abord.'
+            );
         }
         return FastifyServer.instance;
     }
@@ -27,19 +33,31 @@ export class FastifyServer {
     init(): boolean {
         this.server = fastify({});
 
-        this.server.get('/manifest', (_: FastifyRequest, reply: FastifyReply) => {
-            reply.type('application/json').send({
-                version: process.env['MANIFEST_VERSION'] ?? 'v0',
-            });
-        });
+        this.setCors(this.server);
+
+        this.server.get(
+            '/manifest',
+            (_: FastifyRequest, reply: FastifyReply) => {
+                reply.type('application/json').send({
+                    version: process.env['MANIFEST_VERSION'] ?? 'v0',
+                });
+            }
+        );
 
         return !!this.server;
     }
 
+    setCors(server: FastifyInstance) {
+        server.register(cors, {
+            origin: process.env['API_CORS']!,
+            methods: ['POST', 'GET'],
+        });
+    }
+
     async listen(): Promise<string> {
-        return (await this.server!.listen({
+        return await this.server!.listen({
             port: Number(process.env['API_PORT'] ?? '8080'),
             host: '0.0.0.0',
-        }));
+        });
     }
 }
